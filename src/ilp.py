36b87f21d8f299b30aa1a76a986ba3d12eb0c743
fastbodin <aslobodin@proton.me>
2025-02-14 11:55:55 -0800

    updated ilp to reflect change in proof

--- src/ilp.py
+++ src/ilp.py
@@ -9,20 +9,30 @@ def j(i):
 def sum_all(var_type):
     return sum(var_type[i] for i in range(7))
 
-def add_cons_type_14(ilp, vars):
-    ilp += (0 == sum_all(vars['y00105']) + sum_all(vars['y00104']))
+#vars['y22020']
+#vars['y00330']
+#vars['y20120']
+#vars['y02120']
+#vars['y00212']
+#vars['y00213']
+#vars['y20003']
+#vars['y02003']
+#vars['y20004']
+#vars['y02004']
+#vars['y20112']
+#vars['y02112']
+#vars['z20112']
+#vars['z02112']
 
 def add_cons_type_13(ilp, vars):
     ilp += (3*vars['N'] - vars['s'] + 7 ==
             20 + 2*vars['s']+
             4*vars['y22020'] + 4*vars['y20120'] +
-            4*vars['y02120'] + 3*vars['y00330'] +
+            4*vars['y02120'] + 6*vars['y00330'] +
             2*sum_all(vars['y20003']) +
             2*sum_all(vars['y02003']) +
             2*sum_all(vars['y20004']) +
             2*sum_all(vars['y02004']) +
-            2*sum_all(vars['y00105']) +
-            2*sum_all(vars['y00104']) +
             4*sum_all(vars['y00212']) +
             4*sum_all(vars['y00213']) +
             4*sum_all(vars['y20112']) +
@@ -53,8 +63,6 @@ def add_cons_type_10(ilp, vars):
 
 def add_cons_type_9(ilp, vars):
     ilp += (vars['s'] + 7 ==
-            5*sum_all(vars['y00105']) +
-            4*sum_all(vars['y00104']) +
             2*sum_all(vars['y00212']) +
             3*sum_all(vars['y00213']) +
             3*sum_all(vars['y20003']) +
@@ -72,7 +80,6 @@ def add_cons_type_8(ilp, vars):
             vars['y20120'] + vars['y02120'] +
             sum_all(vars['y20003']) +
             sum_all(vars['y02003']) +
-            sum_all(vars['y00104']) +
             sum_all(vars['y00212'])
             )
 
@@ -93,7 +100,6 @@ def add_cons_type_6(ilp, vars):
                 vars['y02112'][h(i)]
                 )
         ilp += (vars['z20112'][i] + vars['z02112'][i] <=
-                vars['y00105'][j(i)] + vars['y00104'][j(i)] +
                 vars['y00212'][j(i)] + vars['y00213'][j(i)] +
                 vars['y20112'][j(i)] + vars['y02112'][j(i)]
                 )
@@ -101,7 +107,6 @@ def add_cons_type_6(ilp, vars):
 def add_cons_type_5(ilp, vars):
     for i in range(7):
         ilp += (vars['y20112'][i] + vars['y02112'][i] <=
-                vars['y00105'][h(i)] + vars['y00104'][h(i)] +
                 vars['y00212'][h(i)] + vars['y00213'][h(i)] +
                 vars['z20112'][h(i)] + vars['z02112'][h(i)]
                 )
@@ -155,7 +160,7 @@ def add_cons_type_3(ilp, vars):
 def add_cons_type_2(ilp, vars):
     for i in range(7):
         ilp += (1 ==
-            vars['y00105'][i] + vars['y00104'][i] + vars['y00212'][i] +
+            vars['y00212'][i] +
             vars['y00213'][i] + vars['y20003'][i] + vars['y02003'][i] +
             vars['y20004'][i] + vars['y02004'][i] + vars['y20112'][i] +
             vars['y02112'][i] + vars['z20112'][i] + vars['z02112'][i]
@@ -171,10 +176,6 @@ def make_variables():
     y02120 = LpVariable('f_{02012}', lowBound=0, cat = 'Integer')
 
     # Variables from Table 2
-    y00105 = [LpVariable('f_{'+str(i)+',00105}', cat = 'Binary')
-              for i in range(7)]
-    y00104 = [LpVariable('f_{'+str(i)+',00104}', cat = 'Binary')
-              for i in range(7)]
     y00212 = [LpVariable('f_{'+str(i)+',00212}', cat = 'Binary')
               for i in range(7)]
     y00213 = [LpVariable('f_{'+str(i)+',00213}', cat = 'Binary')
@@ -200,6 +201,8 @@ def make_variables():
 
     # Variables from Table 4
     s = LpVariable('s', lowBound = 0, cat = 'Integer')
+    a = LpVariable('a', lowBound = 0, cat = 'Integer')
+    b = LpVariable('b', lowBound = 0, cat = 'Integer')
     N = LpVariable('N', lowBound = 0, cat = 'Integer')
 
     # Return all variables in a dict
@@ -208,8 +211,6 @@ def make_variables():
         'y00330': y00330,
         'y20120': y20120,
         'y02120': y02120,
-        'y00105': y00105,
-        'y00104': y00104,
         'y00212': y00212,
         'y00213': y00213,
         'y20003': y20003,
@@ -221,6 +222,8 @@ def make_variables():
         'z20112': z20112,
         'z02112': z02112,
         's': s,
+        'a': a,
+        'b': b,
         'N': N
     }
 
@@ -233,9 +236,9 @@ def check_solve(ilp, solver_name, vars):
     else:
         print(solver_name + ': ' + LpStatus[ilp])
 
-def ilp_1():
+def ilp():
 
-    ilp = LpProblem("ILP_1", LpMaximize)
+    ilp = LpProblem("ILP", LpMinimize)
 
     vars = make_variables()
 
@@ -257,68 +260,26 @@ def ilp_1():
     ilp += vars['N']
 
     # write lp file
-    ilp.writeLP('output/ilp_1.lp')
+    ilp.writeLP('output/ilp.lp')
 
     # Solve ILP
     scip_sol = ilp.solve(getSolver('SCIP_CMD', msg = False,
-                                   logPath="output/scip_ilp_1.log"))
+                                   logPath="output/scip_ilp.log"))
     check_solve(scip_sol, 'SCIP', vars)
 
     gurobi_sol = ilp.solve(getSolver('GUROBI_CMD', msg = False,
-                                     logPath="output/gurobi_ilp_1.log"))
+                                     logPath="output/gurobi_ilp.log"))
     check_solve(gurobi_sol, 'Gurobi', vars)
 
     coin_sol = ilp.solve(getSolver('COIN_CMD', msg = False,
-                                   logPath="output/coin_ilp_1.log"))
-    check_solve(coin_sol, 'COIN_CMD', vars)
-
-def ilp_2():
-
-    ilp = LpProblem("ILP_2", LpMaximize)
-
-    vars = make_variables()
-
-    # Constraint Type 1 is used when defining all variables
-    add_cons_type_2(ilp, vars) # necessary, else always finds sol
-    add_cons_type_3(ilp, vars) # necessary, else always finds sol
-    add_cons_type_4(ilp, vars) # necessary, else always finds sol
-    add_cons_type_5(ilp, vars) # necessary, else always finds sol
-    add_cons_type_6(ilp, vars) # necessary, else always finds sol
-    add_cons_type_7(ilp, vars) # necessary, else unbounded
-    add_cons_type_8(ilp, vars) # necessary, else always finds sol
-    add_cons_type_9(ilp, vars) # necessary, else always finds sol
-    add_cons_type_10(ilp, vars) # necessary, else always finds sol
-    add_cons_type_11(ilp, vars) # unnecessary but helps explore cons_type_12
-    add_cons_type_12(ilp, vars) # necessary, else always finds sol
-    add_cons_type_13(ilp, vars) # necessary, else unbounded
-    add_cons_type_14(ilp, vars)
-
-    # Objective function
-    ilp += vars['N']
-
-    # write lp file
-    ilp.writeLP('output/ilp_2.lp')
-
-    # Solve ILP
-    scip_sol = ilp.solve(getSolver('SCIP_CMD', msg = False,
-                                   logPath="output/scip_ilp_2.log"))
-    check_solve(scip_sol, 'SCIP', vars)
-
-    gurobi_sol = ilp.solve(getSolver('GUROBI_CMD', msg = False,
-                                     logPath="output/gurobi_ilp_2.log"))
-    check_solve(gurobi_sol, 'Gurobi', vars)
-
-    coin_sol = ilp.solve(getSolver('COIN_CMD', msg = False,
-                                   logPath="output/coin_ilp_2.log"))
+                                   logPath="output/coin_ilp.log"))
     check_solve(coin_sol, 'COIN_CMD', vars)
 
 
 def main():
     print("Available solvers on build:", listSolvers(onlyAvailable=True), "\n")
 
-    print("Proof of Lemma 4.33\n")
-    ilp_1()
     print("\nProof of Theorem 4.35\n")
-    ilp_2()
+    ilp()
 
 main()
